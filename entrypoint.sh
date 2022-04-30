179a32f94ea0a1c6957e6b6448f5d35f02f9ad69
wanshao <344277934@qq.com>
2022-04-30 23:57:35 +0800

    fix(project): fix unsafe git error

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ echo "Starting the git Action"
 
 # avoid error fatal: unsafe repository ('/github/workspace' is owned by someone else)
 git config --global --add safe.directory /github/workspace
+git config --global --add safe.directory /github/workspace/themes/next
 
 sh -c "$*"
 
